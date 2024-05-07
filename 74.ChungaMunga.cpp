cc33cd0a6c74e63de1e860475a12a627178f4cc1
devesh <deveshgupta3740@gmail.com>
2024-05-07 15:50:41 +0530

    updated q74

--- 74.ChungaMunga.cpp
+++ 74.ChungaMunga.cpp
@@ -1,11 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
+
 int main(){
-    int x, y, z;
+    ll x, y, z;
     cin >> x >> y >> z;
-    int coconut = (x / z) + (y / z);
-    int rEx = 0;
+    ll coconut = (x / z) + (y / z);
+    ll rEx = 0;
     if((z - (x % z)) <= (y % z)){
         rEx = min((z - (x % z)), (z - (y % z)));
         coconut++;
