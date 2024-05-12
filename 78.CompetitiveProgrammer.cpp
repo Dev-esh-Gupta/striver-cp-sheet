eb88d7ca6564e10f03de90b2f2ee96daa5cc6791
devesh <deveshgupta3740@gmail.com>
2024-05-12 13:46:39 +0530

    updated q78

--- 78.CompetitiveProgrammer.cpp
+++ 78.CompetitiveProgrammer.cpp
@@ -1,10 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
 int main(){
     int n;
     cin >> n;
     while(n--){
-        str
+        string num;
+        cin >> num;
+        bool zero = false;
+        ll sum = 0, even = 0;
+        for(int i=0; i<num.size(); i++){
+            int x = num[i] - '0';
+            if(x == 0) zero = true;
+            if(x%2 == 0) even++;
+            sum += x;
+        }
+        if(sum%3 == 0 and even > 1 and zero) cout << "red" << endl;
+        else cout << "cyan" << endl;
     }
 }
