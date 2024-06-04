7299a0bd5e0ba57f446c9d9eab0058d685d635fa
devesh <deveshgupta3740@gmail.com>
2024-06-04 15:04:01 +0530

    updated q82

--- 82.TheaterSquare.cpp
+++ 82.TheaterSquare.cpp
@@ -1,10 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
+
 int main(){
-    int n, m, a;
+    ll n, m, a;
     cin >> n >> m >> a;
-    int lCount, bCount;
+    ll lCount, bCount;
     lCount = (n % a == 0) ? n / a : n / a + 1;
     bCount = (m % a == 0) ? m / a : m / a + 1;
     cout << lCount * bCount;
