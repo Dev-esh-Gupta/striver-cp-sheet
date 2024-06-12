fbf289b2446f5707f6746330abe21a51aa1946e2
devesh <deveshgupta3740@gmail.com>
2024-06-12 13:23:58 +0530

    updated q98

--- 98.NonCoprimePartition.cpp
+++ 98.NonCoprimePartition.cpp
@@ -8,14 +8,12 @@ int main(){
         cout << "No";
     }else{
         cout << "Yes" << endl;
-        if(n%2 == 0){
-            cout << n - 1 << " " << 1 << endl;
-        }else{
-            cout << n/2 << " " << n - n/2 << endl;
-        }
-        for(int i = 1; i <= n; i++){
+        cout << n - 1 << " " ;
+        for(int i = 1; i < n; i++){
             cout << i << " ";
         }
+        cout <<endl << 1 << " ";
+        cout << n;
     }
     return 0;
 }
