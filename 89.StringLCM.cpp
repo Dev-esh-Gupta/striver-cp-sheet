27bbd8effa37f0629b172b416ff64a816c26f50c
devesh <deveshgupta3740@gmail.com>
2024-06-13 15:09:24 +0530

    Added q89

--- 89.StringLCM.cpp
+++ 89.StringLCM.cpp
@@ -1,18 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int findHCF(int s, int t){
+    if(s > t) swap(s,t);
+    while(t%s != 0){
+        int rem = t % s;
+        t = s;
+        s = rem;
+    }
+    return s;
+}
+
 int main(){
     int q;
     cin >> q;
     while(q--){
         string s, t;
         cin >> s >> t;
-        unordered_map<int, int> count;
+        int m = s.length(), n = t.length();
         
-        if(s.length()%t.length() == 0 || t.length()%s.length() == 0){
+        int hcf = findHCF(m, n);
+        int lcm = (m * n)/hcf;
 
+        string res1 = "", res2 = "";
+        while(res1.length() != lcm){
+            res1 += s;
+        }
+        while(res2.length() != lcm){
+            res2 += t;
+        }
+        if(res1 == res2){
+            cout << res1 << endl;
         }else{
-            cout << -1;
+            cout << -1 << endl;
         }
     }
+    return 0;
 }
