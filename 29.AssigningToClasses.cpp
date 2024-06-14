3d6da775ecc67661f28241991281f763dc60d2b5
devesh <deveshgupta3740@gmail.com>
2024-06-14 07:21:50 +0530

    Updated q29

--- 29.AssigningToClasses.cpp
+++ 29.AssigningToClasses.cpp
@@ -4,4 +4,16 @@ using namespace std;
 int main(){
     int t;
     cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> ele;
+        for(int i=0; i<2*n; i++){
+            int num;
+            cin >> num;
+            ele.push_back(num);
+        }
+        sort(ele.begin(), ele.end());
+        cout << abs(ele[n-1] - ele[n]) << endl;
+    }
 }
