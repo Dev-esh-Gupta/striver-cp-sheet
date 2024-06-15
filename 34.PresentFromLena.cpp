def18fa1cea5a30b779b893395320cf69c4ac985
devesh <deveshgupta3740@gmail.com>
2024-06-15 22:49:14 +0530

    updated q34

--- 34.PresentFromLena.cpp
+++ 34.PresentFromLena.cpp
@@ -4,11 +4,45 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    for(int i=1; i<=2*n+1; i++){
+    int gap = n;
+    int run = 1;
+    for(int i=0; i<2*n+1; i++){
         // space printing
-        for(int j=0; j<i; j++){
-            cout << j%(n+1) << " ";
+        if(i <= n){
+            int j = run;
+            for(int k=0; k<gap; k++){
+                cout << "  ";
+            }
+            for(int k=0; k<(i+1); k++){
+                cout << k << " ";
+                j--;
+            }
+            while(j != 0){
+                j--;
+                cout << j << " ";
+            }
+            // while()
+            if(i != n){
+                gap--;
+                run += 2;
+            }
+            cout << endl;
+        }else{
+            gap++;
+            run -= 2;
+            int j = run;
+            for(int k=0; k<gap; k++){
+                cout << "  ";
+            }
+            for(int k=0; k<run/2; k++){
+                cout << k << " ";
+                j--;
+            }
+            while(j != 0){
+                j--;
+                cout << j << " ";
+            }
+            cout << endl;
         }
-        cout << endl;
     }
 }
