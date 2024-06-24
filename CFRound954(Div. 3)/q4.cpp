8db2360065f781480f4322b446248ba45149c035
devesh <deveshgupta3740@gmail.com>
2024-06-24 12:14:19 +0530

    Update q4 of CFRound954

--- CFRound954(Div. 3)/q4.cpp
+++ CFRound954(Div. 3)/q4.cpp
@@ -1,6 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void findMinSum(string &s, int n){
+    if(n<=2){
+            cout << stoi(s)<<endl;
+            return;
+        }
+    int ans = INT_MAX;
+    for(int i=0; i<n-1; i++){
+        int twoDig = (s[i]-'0')*10 + (s[i+1]-'0');
+        int sumExceptOne = 0;
+        int mini = 9;
+        int finalSum;
+        for(int j=0; j<n; j++){
+            if(j != i && j != i+1){
+                mini = min(mini, (s[j]-'0'));
+                if(s[j] != '1') sumExceptOne += (s[j] - '0');
+            }
+        }
+        if(mini == 0){
+            cout << 0 << endl;
+            return;
+        }
+        
+        if(sumExceptOne == 0){
+            sumExceptOne = 1;
+            finalSum = twoDig;
+        }else{
+            finalSum = twoDig==1?sumExceptOne:sumExceptOne + twoDig;
+        }
+        ans = min(ans, finalSum);
+    }
+    cout << ans << endl;
+    return;
+}
+
 int main(){
     int t;
     cin >> t;
@@ -8,33 +42,7 @@ int main(){
         int n;
         string s;
         cin >> n >> s;
-        if(n <= 2){
-            cout << s << endl;
-        }else{
-            bool flag = false;
-            int sum = 0;
-            for(auto &ch : s){
-                if(ch == '0'){
-                    flag = true;
-                }
-                sum += (ch - '0');
-            }
-            if(flag){
-                cout << 0 << endl;
-                break;
-            }else{
-                int mini = 99;
-                for(int i=0; i<n-1; i++){
-                    int num = (s[i]-'0')*10 + (s[i+1]-'0');
-                    mini = min(num, mini);
-                }
-                if(mini == 11){
-                    cout << mini << endl;
-                }else{
-                    cout << sum + mini - (mini%10) - (mini/10) << endl;
-                }
-            }
-
-        }
+        findMinSum(s, n);
     }
+    return 0;
 }
